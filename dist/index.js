b4cb0ab8f7a241113b27feeb09154869049d73b2
pengyunfei <pengyunfei@360.cn>
2023-09-27 16:33:52 +0800

    feat: rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
-let m = (e = 21) => crypto.getRandomValues(new Uint8Array(e)).reduce((t, r) => (r &= 63, r < 36 ? t += r.toString(36) : r < 62 ? t += (r - 26).toString(36).toUpperCase() : r > 62 ? t += "-" : t += "_", t), ""), l = null, a = null, s = [], c = [], g = (e) => {
+let m = (e = 21) => crypto.getRandomValues(new Uint8Array(e)).reduce((t, r) => (r &= 63, r < 36 ? t += r.toString(36) : r < 62 ? t += (r - 26).toString(36).toUpperCase() : r > 62 ? t += "-" : t += "_", t), ""), n = null, a = null, s = [], c = [], g = (e) => {
   e.preventDefault();
 };
 const v = () => {
-  l.style.position = "absolute", l.style.top = "-100vh", l.style.left = "-100vw", l.style.pointerEvents = "none", l.id = m(), a = l.id, document.body.appendChild(l);
+  n.style.position = "absolute", n.style.top = "-100vh", n.style.left = "-100vw", n.style.pointerEvents = "none", n.id = m(), a = n.id, document.body.appendChild(n);
 }, f = () => {
   let e = document.getElementById(a);
   e == null || e.remove();
@@ -12,36 +12,36 @@ let u = (e) => {
     t.removeEventListener("dragover", g), t.classList.remove(`${a}-active`);
   }), c.forEach((t) => {
     t.remove();
-  }), s = [], c = [], f(), l = null, a = null;
+  }), s = [], c = [], f(), n = null, a = null;
 }, p = (e) => {
   let t = e.clonedStyle || {};
-  d(t) || Object.keys(t).forEach((n) => {
-    l.style[n] = t[n];
+  d(t) || Object.keys(t).forEach((l) => {
+    n.style[l] = t[l];
   });
   let r = e.targetStyle || {};
   if (!d(r)) {
-    let n = document.createElement("style");
-    n.type = "text/css";
+    let l = document.createElement("style");
+    l.type = "text/css";
     let o = "";
     Object.keys(r).forEach((i) => {
       o += `${h(i)}:${r[i]};`;
-    }), n.innerHTML = `.${a}-active {${o}} `, document.head.appendChild(n), c.push(n);
+    }), l.innerHTML = `.${a}-active {${o}} `, document.head.appendChild(l), c.push(l);
   }
 }, y = (e = []) => {
   e.forEach((t) => {
-    (E(t) ? [t] : document.querySelectorAll(t) || []).forEach((n) => {
-      n.classList.add(`${a}-active`), n.addEventListener("dragover", g), s.push(n);
+    (E(t) ? [t] : document.querySelectorAll(t) || []).forEach((l) => {
+      l.classList.add(`${a}-active`), l.addEventListener("dragover", g), s.push(l);
     });
   });
 };
 const D = (e, t) => (r) => {
-  let n = r.target;
-  l = n.cloneNode(!0), v(), d(t) || p(t), r.dataTransfer.setDragImage(l, 100, 100), n.addEventListener("dragend", u), r.remember = (o) => {
+  let l = r.target;
+  n = l.cloneNode(!0), v(), d(t) || p(t), r.dataTransfer.setDragImage(n, 100, 100), l.addEventListener("dragend", u), r.remember = (o) => {
     r.dataTransfer.setData("text/plain", JSON.stringify(o));
   }, r.activeTarget = y, e(r);
 };
 function E(e) {
-  return e instanceof HTMLElement;
+  return console.log("obj :>> ", e), e instanceof HTMLElement;
 }
 function d(e) {
   return Object.keys(e).length === 0;
@@ -49,7 +49,7 @@ function d(e) {
 let h = (e) => e.replace(/([A-Z])/g, "-$1").toLowerCase();
 export {
   v as addDragNode,
-  l as cloneDom,
+  n as cloneDom,
   D as dragP,
   f as removeDragNode
 };
